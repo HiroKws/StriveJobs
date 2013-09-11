520ce388816597d48aed41a4afc5e92738452f00
Hirohisa Kawase <hirokws@gmail.com>
2013-09-11 12:14:45 +0900

    Initial commit

---
+++ src/Hirokws/Strivejobs/StrivejobsServiceProvider.php
@@ -0,0 +1,45 @@
+<?php namespace Hirokws\Strivejobs;
+
+use Illuminate\Support\ServiceProvider;
+
+class StrivejobsServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+        // Defined prefix for config, lang, views.
+		$this->package('hirokws/strivejobs', 'SJ');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
